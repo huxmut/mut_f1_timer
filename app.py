e0112975175d2c4755d0f74be6f1c35ab380fe22
huxmut <69717136+huxmut@users.noreply.github.com>
2026-02-21 21:44:52 +1000

    Update app.py

--- app.py
+++ app.py
@@ -7,9 +7,9 @@ import os
 app = Flask(__name__)
 
 # Image settings
-WIDTH = 900
-HEIGHT = 250
-FONT_SIZE = 90
+WIDTH = 800
+HEIGHT = 139
+FONT_SIZE = 70
 
 FRAME_COUNT = 60          # 60 seconds animation
 FRAME_DURATION = 1000     # 1 second per frame
@@ -123,5 +123,6 @@ def countdown_gif():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
 
 
